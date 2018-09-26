04974b73000d2b1e7c028449edcd10c9f94c45b6
Kayle7777 <jessewebb2@gmail.com>
2018-09-25 19:22:15 -0500

    Move from lp to dt

--- _craft_search.c
+++ _craft_search.c
@@ -6,11 +6,13 @@ pfound = #134933:_check_contents(searchitem.name, player.contents);
 ptf = pfound[1] ? 1 | 0;
 if (ptf)
     player:tell("Searching self...");
-    return player:queue_action($actions.get, {{searchitem}, player});
+    player:queue_action($actions.get, {{searchitem}, {}}, player);
 elseif (pfound[2])
     for pcontainers in (pfound[2])
         player:tell("Searching self containers...");
-        return player:queue_action($actions.get, {{searchitem}, pcontainers, 1, tostr("get ", searchitem.name, " from ", pcontainers.name)});
+        player:queue_action($actions.get, {{$mu:match(searchitem.name, pcontainers.contents)}, pcontainers, {}}, 1, tostr("get ", searchitem.name, " from ", pcontainers.name));
+        suspend(1);
+        player:queue_action($actions.put, {{$mu:match(searchitem.name, player.contents)}, this, {}}, 1, tostr("put ", searchitem.name, " in ", this.name));
     endfor
 else
     rfound = #134933:_check_contents(searchitem.name, player.location.contents);
@@ -18,14 +20,18 @@ else
     if (roomtf)
         player:tell("Searching room...");
         player:tell(tostr("get ", searchitem.name));
-        return player:queue_action($actions.get, {{searchitem}, player.location, 1, tostr("get ", searchitem.name)});
+        player:queue_action($actions.get, {{$mu:match(searchitem.name, player.contents)}, player.location, {}}, 1, tostr("get ", searchitem.name));
+        suspend(1);
+        player:queue_action($actions.put, {{$mu:match(searchitem.name, player.contents)}, this, {}}, 1, tostr("put ", searchitem.name, " in ", this.name));
     else
         for containers in (rfound[2])
             cfound = #134933:_check_contents(searchitem.name, containers.contents);
             c1tf = cfound[1] ? 1 | 0;
             if (c1tf)
                 player:tell("Searching container1...");
-                return player:queue_action($actions.get, {{searchitem}, containers, 1, tostr("get ", searchitem.name, " from ", containers.name)});
+                player:queue_action($actions.get, {{$mu:match(searchitem.name, containers.contents)}, containers, {}}, 1, tostr("get ", searchitem.name, " from ", containers.name));
+                suspend(1);
+                player:queue_action($actions.put, {{$mu:match(searchitem.name, player.contents)}, this, {}}, 1, tostr("put ", searchitem.name, " in ", this.name));
             endif
         endfor
     endif
